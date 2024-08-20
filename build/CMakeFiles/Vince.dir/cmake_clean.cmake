0dd305025952c9983cf71a594dd5369653bab042
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-20 23:55:02 +0300

    Init build

---
+++ build/CMakeFiles/Vince.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vince.dir/src/main.cpp.obj"
+  "CMakeFiles/Vince.dir/src/main.cpp.obj.d"
+  "Vince.exe"
+  "Vince.exe.manifest"
+  "Vince.pdb"
+  "libVince.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vince.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

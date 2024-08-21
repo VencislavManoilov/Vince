884a128898144f28f6952eab1957d44725fe4c07
VencislavManoilov <vencislav2.manoilov@gmail.com>
2024-08-21 18:52:54 +0300

    Something is working I guess

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "My Browser");
+    gtk_window_set_title(GTK_WINDOW(window), "Vince");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     gtk_widget_show_all(window);
 }
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("com.example.MyBrowser", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("com.example.Vince", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);

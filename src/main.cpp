7e1421524956f56deac6e5bdedd0163eb61a6913
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-21 14:41:02 +0300

    I start to understand how it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <gtk/gtk.h>
 
-int main(int argc, char* argv[]) {
-    gtk_init(&argc, &argv);
-
-    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Vince");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
-
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
+static void on_activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "My Browser");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     gtk_widget_show_all(window);
-    gtk_main();
+}
 
-    return 0;
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("com.example.MyBrowser", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }

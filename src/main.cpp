40b199012aaaee4aa8716ce2a58be6857c73f0f1
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-22 21:42:12 +0300

    I don't know what is going on! Why this doesn't work on Windows! I wanna die

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,13 @@
-#include <iostream>
-#include "cef_app.h"
-#include "cef_browser.h"
-#include "cef_command_line.h"
-
-class SimpleApp : public CefApp, public CefBrowserProcessHandler {
-public:
-    SimpleApp() {}
-
-    CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
-        return this;
-    }
-
-    void OnContextInitialized() override {
-        CefWindowInfo window_info;
-        CefBrowserSettings browser_settings;
-
-        window_info.SetAsPopup(NULL, "CEF Simple");
-
-        CefBrowserHost::CreateBrowser(window_info, new SimpleApp(), "https://www.google.com", browser_settings, nullptr, nullptr);
-    }
-
-private:
-    IMPLEMENT_REFCOUNTING(SimpleApp);
-};
+#include <QApplication>
+#include <QWebEngineView>
 
 int main(int argc, char *argv[]) {
-    CefEnableHighDPISupport();
+    QApplication app(argc, argv);
 
-    CefMainArgs main_args(argc, argv);
-    CefRefPtr<SimpleApp> app(new SimpleApp);
+    QWebEngineView view;
+    view.setUrl(QUrl("http://google.com"));
+    view.resize(1024, 768);
+    view.show();
 
-    int exit_code = CefExecuteProcess(main_args, app, nullptr);
-    if (exit_code >= 0) {
-        return exit_code;
-    }
-
-    CefSettings settings;
-    CefInitialize(main_args, settings, app, nullptr);
-
-    CefRunMessageLoop();
-
-    CefShutdown();
-    return 0;
+    return app.exec();
 }

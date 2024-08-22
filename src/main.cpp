4385526a3d82d25490760371795dd35c5b80d153
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-22 15:27:22 +0300

    I am working too much for it to not work!

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,45 @@
-#include <gtk/gtk.h>
+#include <iostream>
+#include "cef_app.h"
+#include "cef_browser.h"
+#include "cef_command_line.h"
 
-static void on_activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Vince");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-    gtk_widget_show_all(window);
-}
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+public:
+    SimpleApp() {}
 
-int main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
+    CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
+        return this;
+    }
 
-    app = gtk_application_new("com.example.Vince", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    void OnContextInitialized() override {
+        CefWindowInfo window_info;
+        CefBrowserSettings browser_settings;
 
-    return status;
+        window_info.SetAsPopup(NULL, "CEF Simple");
+
+        CefBrowserHost::CreateBrowser(window_info, new SimpleApp(), "https://www.google.com", browser_settings, nullptr, nullptr);
+    }
+
+private:
+    IMPLEMENT_REFCOUNTING(SimpleApp);
+};
+
+int main(int argc, char *argv[]) {
+    CefEnableHighDPISupport();
+
+    CefMainArgs main_args(argc, argv);
+    CefRefPtr<SimpleApp> app(new SimpleApp);
+
+    int exit_code = CefExecuteProcess(main_args, app, nullptr);
+    if (exit_code >= 0) {
+        return exit_code;
+    }
+
+    CefSettings settings;
+    CefInitialize(main_args, settings, app, nullptr);
+
+    CefRunMessageLoop();
+
+    CefShutdown();
+    return 0;
 }

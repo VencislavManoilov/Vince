e995a1fef594f7f1c497cf73bfb11fb708902f2e
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-08-24 16:58:48 +0300

    GOOOOOOOGLE IS SHOWING ONNNNNN

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QWebEngineView>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
     QMainWindow window;
     window.resize(1024, 768);
+
+    // Create a QWebEngineView and set it as the central widget
+    QWebEngineView* view = new QWebEngineView(&window);
+    view->setUrl(QUrl("https://www.google.com"));
+    window.setCentralWidget(view);
+
     window.show();
 
     return app.exec();
